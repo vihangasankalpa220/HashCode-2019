e884cd71dcd197390f1e33da340e306847cd9a5a
Asiri Hewage <foolmashi@gmail.com>
2019-02-20 15:42:47 +0530

    Update solution1-asiri.py

--- Pizza/solution1-asiri.py
+++ Pizza/solution1-asiri.py
@@ -1,1 +1,11 @@
 print("Pizza problem")
+try:
+    R,C,MIN,MAX = map(int,input().split()) 
+    pizza = [[0] * R] * C
+    print(pizza)
+except:
+    print("Error!")
+
+##Mushroom -> M | Tomato -> T
+
+
